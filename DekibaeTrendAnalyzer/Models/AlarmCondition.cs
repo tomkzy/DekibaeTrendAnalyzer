a88b1e58476c1a861dc6c79f188d4a727f86f395
Kazuya Tomono <hujikitahotaka@gmail.com>
2025-07-27 13:20:44 +0900

    Initial commit

---
+++ DekibaeTrendAnalyzer/Models/AlarmCondition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DekibaeTrendAnalyzer.Models
+{
+    public class AlarmCondition
+    {
+        public int MinConsecutiveCount { get; set; }
+        public int MinDefectCount { get; set; }
+    }
+
+}
